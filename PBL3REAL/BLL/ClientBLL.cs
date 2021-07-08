ce8176ab98e74eb4545da1dea9d61843748c70b1
thanhnhat108 <thanhnhatpct108@gmail.com>
2021-07-08 11:18:12 +0700

    hoan thien cac chuc nang cuoi cung cua ClientBLL

--- PBL3REAL/BLL/ClientBLL.cs
+++ PBL3REAL/BLL/ClientBLL.cs
@@ -54,6 +54,49 @@ namespace PBL3REAL.BLL
             }
         }
 
+        public List<ClientVM> findByProperty(int pages, int rows, Dictionary<string, string> properties, string orderBy)
+        {
+            int start = (pages - 1) * rows;
+            int length = rows;
+            List<ClientVM> listVm = new List<ClientVM>();
+            foreach (Client client in ClientDAL.Instance.findByProperty(start, length, properties, orderBy))
+            {
+                ClientVM clientVM = mapper.Map<ClientVM>(client);
+                clientVM.Index = ++start;
+                listVm.Add(clientVM);
+            }
+            return listVm;
+        }
+
+        public bool checkexisted(Dictionary<string, string> properties)
+        {
+            var list = ClientDAL.Instance.checkExisted(properties);
+            if (list.Count == 0) return true;
+            return false;
+        }
+
+        public ClientVM findById(int id)
+        {
+            Client client = ClientDAL.Instance.findById(id);
+            ClientVM clientVM = mapper.Map<ClientVM>(client);
+            return clientVM;
+        }
+
+        public int getPagination(int rows, Dictionary<string, string> properties)
+        {
+            int totalRows = ClientDAL.Instance.getTotalRow(properties);
+            int totalpage;
+            if (totalRows % rows == 0)
+            {
+                totalpage = totalRows / rows;
+            }
+            else
+            {
+                totalpage = totalRows / rows + 1;
+            }
+            return totalpage;
+        }
+
 
     }
 }
