055abb171e0dfa92b0898a8659a9ddacba9fc199
thanhnhat108 <thanhnhatpct108@gmail.com>
2021-07-08 11:16:23 +0700

    lam phan ClientBLL voi cac chuc nang CRUD

---
+++ PBL3REAL/BLL/ClientBLL.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using PBL3REAL.DAL;
+using PBL3REAL.Model;
+using PBL3REAL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PBL3REAL.BLL
+{
+    public class ClientBLL
+    {
+        private Mapper mapper;
+        public ClientBLL()
+        {
+            mapper = new Mapper(MapperVM.config);
+        }
+
+        public void add(ClientVM clientVM)
+        {
+            Client client = new Client();
+            mapper.Map(clientVM, client);
+            ClientDAL.Instance.add(client);
+        }
+
+        public void update(ClientVM clientVM)
+        {
+            Client client = new Client();
+            mapper.Map(clientVM, client);
+            ClientDAL.Instance.update(client);
+        }
+        public void delete(int id)
+        {
+            try
+            {
+                ClientDAL.Instance.delete(id);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void restore(int id)
+        {
+            try
+            {
+                ClientDAL.Instance.restore(id);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ PBL3REAL/ViewModel/ClientVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PBL3REAL.ViewModel
+{
+   public class ClientVM
+    { 
+        public int Index { get; set; }
+        public int IdClient { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Code { get; set; }
+        public string Gmail { get; set; }
+
+        public bool CliActiveflag { get; set; }
+    }
+}
